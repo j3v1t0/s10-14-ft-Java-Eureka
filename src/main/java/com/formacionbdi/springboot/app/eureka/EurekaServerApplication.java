0f68be65f1ade62a451165ddaa1049eac6744673
Alfonso Almonte <alfonsoalmonte@gmail.com>
2023-08-23 00:55:14 -0400

    Changing configuration

--- src/main/java/com/formacionbdi/springboot/app/eureka/EurekaServerApplication.java
+++ src/main/java/com/formacionbdi/springboot/app/eureka/EurekaServerApplication.java
@@ -1,12 +1,17 @@
 package com.formacionbdi.springboot.app.eureka;
 
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.scheduling.annotation.Scheduled;
 
 @EnableEurekaServer
 @SpringBootApplication
 public class EurekaServerApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaServerApplication.class, args);
 	}
